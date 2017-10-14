aeb77df5b9961710cdd7f9fb2a70af7fbe09e2aa
Natalia Ramirez <natalia@nat-oitwireless-inside-vapornet100-b-27683.Princeton.EDU>
2017-10-14 17:54:06 -0400

    Drivers guide URL fix

--- src/components/DriversGuidePage.js
+++ src/components/DriversGuidePage.js
@@ -8,7 +8,7 @@ class DriversGuidePage extends Component {
             <p className="welcome-lead">
                 En esta pagina usted puede encontrar la guia de manejo del Estado de Washington Department of Licensing.
                 Es muy importante que estudie cada pagina de esta guia, desde la portada hasta la contraportada, por que las preguntas de los examenes estan totalmente basadas en el contenido de esta guia.</p>
-            <object className="drivers-guide-object" width="600" height="800" data="https://www.dol.wa.gov/driverslicense/docs/driverguide-spanish.pdf">
+            <object className="drivers-guide-object" width="600" height="800" data="http://www.dol.wa.gov/driverslicense/docs/driverguide-spanish.pdf">
             No se pudo cargar la guia de manejo. Por favor intentelo mas tarde.
             </object>
         </div>
