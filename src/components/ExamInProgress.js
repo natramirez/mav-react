aee828f9b271469c502ce7661e60f6e1924acc86
Natalia Ramirez <natalia@nat-oitwireless-inside-vapornet100-b-27683.Princeton.EDU>
2017-10-13 20:21:17 -0400

    url fix #2

--- src/components/ExamInProgress.js
+++ src/components/ExamInProgress.js
@@ -29,7 +29,7 @@ class ExamInProgress extends Component {
     componentWillMount() {
         var port = process.env.API_PORT || 3001;
         var host = window.location.hostname;
-        var serverURL = 'http://' + host + ':' + port + '/api/questions';
+        var serverURL = 'http://' + host + '/api/questions';
         axios.get(serverURL,{
             params: {
               numQuestions: this.props.numQuestions
