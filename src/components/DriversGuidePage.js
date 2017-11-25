aa743f0438c1617ded183e624684e33eeb76790d
Natalia Ramirez <natalia@Natalias-MacBook-Pro.local>
2017-11-25 13:45:49 -0800

    Now serving static drivers-guide file

--- src/components/DriversGuidePage.js
+++ src/components/DriversGuidePage.js
@@ -8,8 +8,7 @@ class DriversGuidePage extends Component {
             <p className="welcome-lead">
                 En esta pagina usted puede encontrar la guia de manejo del Estado de Washington Department of Licensing.
                 Es muy importante que estudie cada pagina de esta guia, desde la portada hasta la contraportada, por que las preguntas de los examenes estan totalmente basadas en el contenido de esta guia.</p>
-            <object className="drivers-guide-object" width="600" height="800" data="http://www.dol.wa.gov/driverslicense/docs/driverguide-spanish.pdf">
-            No se pudo cargar la guia de manejo. Por favor intentelo mas tarde.
+            <object className="drivers-guide-object" width="600" height="500" data="../driver-guide-spanish.pdf">
             </object>
         </div>
     );
