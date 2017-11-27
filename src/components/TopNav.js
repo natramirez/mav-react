0710ff3f37e8898b9384beffc59d40253560e53f
Natalia Ramirez <natalia@Natalias-MacBook-Pro.local>
2017-11-26 21:32:53 -0800

    began implementing responsive navigation browser and other styling updates

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -3,21 +3,40 @@ import { NavLink } from 'react-router-dom';
 import '../App.css';
 
 class TopNav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.getInitialState();
+    this.setClass.bind(this);
+  }
+  getInitialState() {
+    return {
+      responsiveClass: false
+    }
+  }
+  toggleClassBool() {
+    this.setState({responsiveClass: this.state.responsiveClass ? false : true})
+  }
+  setClass() {
+    return (this.state.responsiveClass ?' responsive':'');
+  }
   render() {
     return (
     <div className="top-nav">
-        <ul className="top-nav-list">
+        <ul className={"top-nav-list" + this.setClass()}>
             <li className='nav-tabs'>
               <NavLink exact to="/">Inicio</NavLink>
             </li>
             <li className='nav-tabs'>
               <NavLink to="/examen_completo">Examen completo</NavLink>
             </li>
-            <li className='nav-tabs'>
+            {/* <li className='nav-tabs'>
               <NavLink to="/examen_express">Examen express</NavLink>
-            </li>
+            </li> */}
             <li className='nav-tabs'>
               <NavLink to="/guia_de_manejo">Guia de manejo</NavLink>
+            </li>
+            <li className='nav-icon' onClick={this.toggleClassBool.bind(this)}>
+            &#9776;
             </li>
         </ul>
     </div>
