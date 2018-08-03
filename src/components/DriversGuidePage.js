ae6afb9198ea7d1b888c2f7f2a9b1336b7f338c2
natramirez <nataliar1596@gmail.com>
2018-08-02 20:00:31 -0700

    Showing local drivers guide pdf

--- src/components/DriversGuidePage.js
+++ src/components/DriversGuidePage.js
@@ -10,7 +10,9 @@ class DriversGuidePage extends Component {
             <p className="welcome-lead">
                 En esta pagina usted puede encontrar la guia de manejo del Estado de Washington Department of Licensing.
                 Es muy importante que estudie cada pagina de esta guia, desde la portada hasta la contraportada, por que las preguntas de los examenes estan totalmente basadas en el contenido de esta guia.</p>
-            <iframe className="drivers-guide-object" src="http://docs.google.com/gview?url=http://www.dol.wa.gov/driverslicense/docs/driverguide-spanish.pdf&embedded=true" frameBorder="0"></iframe>
+            {/* <iframe className="drivers-guide-object" src="http://docs.google.com/gview?url=http://www.dol.wa.gov/driverslicense/docs/driverguide-spanish.pdf&embedded=true" frameBorder="0"></iframe> */}
+            <iframe className="drivers-guide-object" src="../driver-guide-spanish.pdf" frameBorder="0"></iframe>
+
         </div>
     );
   }
